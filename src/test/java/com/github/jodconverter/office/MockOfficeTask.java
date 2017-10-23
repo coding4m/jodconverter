3f92e284ac03f913ab1e3812858f8ebbd2986e7a
coding4m <coding4m@gmail.com>
2017-10-23 11:30:05 +0800

    1. refactor.

---
+++ src/test/java/com/github/jodconverter/office/MockOfficeTask.java
@@ -0,0 +1,56 @@
+//
+// JODConverter - Java OpenDocument Converter
+// Copyright 2004-2012 Mirko Nasato and contributors
+//
+// JODConverter is Open Source software, you can redistribute it and/or
+// modify it under either (at your option) of the following licenses
+//
+// 1. The GNU Lesser General Public License v3 (or later)
+//    -> http://www.gnu.org/licenses/lgpl-3.0.txt
+// 2. The Apache License, Version 2.0
+//    -> http://www.apache.org/licenses/LICENSE-2.0.txt
+//
+package com.github.jodconverter.office;
+
+import static com.github.jodconverter.office.OfficeUtils.*;
+
+import com.sun.star.beans.PropertyValue;
+import com.sun.star.frame.XComponentLoader;
+import com.sun.star.lang.XComponent;
+import com.sun.star.util.XCloseable;
+
+public class MockOfficeTask implements OfficeTask {
+
+    private long delayTime = 0L;
+
+    private boolean completed = false;
+
+    public MockOfficeTask() {
+        // default
+    }
+
+    public MockOfficeTask(long delayTime) {
+        this.delayTime = delayTime;
+    }
+
+    public void execute(OfficeContext context) throws OfficeException {
+        XComponentLoader loader = cast(XComponentLoader.class, context.getService(SERVICE_DESKTOP));
+        assert loader != null : "desktop object is null";
+        try {
+            PropertyValue[] arguments = new PropertyValue[] { property("Hidden", true) };
+            XComponent document = loader.loadComponentFromURL("private:factory/swriter", "_blank", 0, arguments);
+            if (delayTime > 0) {
+                Thread.sleep(delayTime);
+            }
+            cast(XCloseable.class, document).close(true);
+            completed = true;
+        } catch (Exception exception) {
+            throw new OfficeException("failed to create document", exception);
+        }
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+}
